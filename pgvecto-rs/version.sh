9665b509c8135945115a123779e7f86e239c0c9e
yellowhat <1692490+yellowhat@users.noreply.github.com>
2025-05-24 17:22:07 +0200

    Remove pgvecto-rs as using upstream

--- pgvecto-rs/version.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# How to gather version
-set -euo pipefail
-
-rs_ver=$(grep FROM Dockerfile | head -1 | cut -d ":" -f 2 | cut -d "-" -f 2)
-pg_ver=$(grep FROM Dockerfile | tail -1 | cut -d ":" -f 2)
-
-echo "${rs_ver}-${pg_ver}"
